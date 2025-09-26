b38107b9b17c5fa28b48b95000d34848b5aed392
nasserpk <nparkuk@gmail.com>
2025-09-26 19:36:00 +0330

    add profile data and profile repository

--- lib/features/home/repositories/profile_repository.dart
+++ lib/features/home/repositories/profile_repository.dart
@@ -1,3 +1,19 @@
+import 'package:hotelino_app/features/home/data/models/profile.dart';
+
 class ProfileRepository {
-  fetchUserProfile() {}
+  fetchUserProfile() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    return Profile(
+      id: "7954862145",
+      name: "کیان خاکی",
+      email: "kiankhaki@gmail.com",
+      avatarUrl: "https://dunijet.ir/content/projects/hotelino/profile_pic.png",
+      phoneNumber: "+989123456789",
+      location: "تهران، ایران",
+      bio: "عاشق سفر و تجربه هتل‌های لاکچری 🌍✨",
+      bookings: 12,
+      favorites: 5,
+      notifications: 3,
+    );
+  }
 }
