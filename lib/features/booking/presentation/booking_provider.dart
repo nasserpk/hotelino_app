d1d6c5062b8f6017d71f41b5196a0f299499710c
nasserpk <nparkuk@gmail.com>
2025-10-20 19:08:55 +0330

    add booking provider

---
+++ lib/features/booking/presentation/booking_provider.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:hotelino_app/features/booking/data/models/booking_model.dart';
+import 'package:persian_datetime_picker/persian_datetime_picker.dart';
+
+class BookingProvider with ChangeNotifier {
+  final BookingModel _booking = BookingModel();
+  BookingModel get booking => _booking;
+
+  void setName(String value) {
+    _booking.fullName = value;
+    notifyListeners();
+  }
+
+  void setDestination(String value) {
+    _booking.destination = value;
+    notifyListeners();
+  }
+
+  void setPhoneNumber(String value) {
+    _booking.phoneNumber = value;
+    notifyListeners();
+  }
+
+  void setNumberOfGuests(String value) {
+    _booking.numberOfGuests = value;
+    notifyListeners();
+  }
+
+  void setRangeDate(JalaliRange value) {
+    _booking.checkInOutRangeDate = value;
+    notifyListeners();
+  }
+}
